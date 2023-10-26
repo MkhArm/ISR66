6c2b05d232e4b842574ef730ac85f6a684f35fd2
PCAdmin <mkhoyanAr@gmail.com>
2023-10-27 00:41:49 +0300

    change and use cringeAReturn and ivanPrivet method

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
+        System.out.println(cringeAReturn(10));
+        ivanPrivet();
     }
 
     //Самый полезный метод в истории человечества
-    public int cringeAReturn(int a){
+    public static int cringeAReturn(int a){
         return a;
     }
 
     //Привет Иван
-    public void ivanPrivet(){
+    public static void ivanPrivet(){
         System.out.println("Ivan Privet");
     }
 }
