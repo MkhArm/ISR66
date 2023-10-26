d7fe108d288591b5b0cf5bde5a9907edd6e8d2ef
PCAdmin <mkhoyanAr@gmail.com>
2023-10-27 00:40:20 +0300

    add comments

--- Main.java
+++ Main.java
@@ -3,10 +3,12 @@ class Main{
         System.out.print("Это добавлено в ветке dev");
     }
 
+    //Самый полезный метод в истории человечества
     public int cringeAReturn(int a){
         return a;
     }
 
+    //Привет Иван
     public void ivanPrivet(){
         System.out.println("Ivan Privet");
     }
